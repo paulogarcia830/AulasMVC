537080bb530fc31969af3566a41c51182e124711
paulogarcia830 <paulogarcia830@gmail.com>
2021-05-05 19:21:07 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Paulo ficando fera no MVC.";
+            ViewData["email"] = "paulogarcia830@gmail.com";
 
             return View();
         }
